07b71f48f764ed4190ee7f8c6774f854c5f41a1b
Joe Ganobsik <jganobsik@gmail.com>
2019-02-10 19:37:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ let newID = oldID + 1000000000;
 let ageIsValid = Number.isInteger(currentAge);
 
 let randomNumber = Math.random(1,20);
-let randomInteger = Math.round(randomNumber);
+let randomInteger = Math.floor(randomNumber);
 let randomUserID = randomInteger + 1000000000;
